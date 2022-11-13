30120e6277803a56929799d3dd614d633ff6d16b
wmertens <Wout.Mertens@gmail.com>
2022-11-13 13:28:21 +0100

    add qwik-styled-ve

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { qwikStyledVEPlugin } from 'qwik-styled-ve';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikStyledVEPlugin()],
   };
 });
